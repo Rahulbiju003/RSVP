dda18541156d876bc14807d018e1546bf200906d
Shreejit03 <116968835+Shreejit03@users.noreply.github.com>
2023-05-26 20:18:34 +0530

    Update main.js

--- Main/main.js
+++ Main/main.js
@@ -79,32 +79,6 @@ app.get('/', async (req, res) => {
     res.json(response);
 });
 
-
-
-
-// Require our ORM
-const ORM = require('some-orm-library');
-
-// Create a connection to our database
-const db = await ORM.connect('localhost', 'databaseName');
-
-// Create our route for linking JS and a database
-app.get('/', async (req, res) => {
-    // Fetch data from the database
-    let response = await db.findAll('UserTable');
-    // Return the data in the response
-    res.json(response);
-});
-what is ORM
-
-Object-relational mapping (ORM) is a process of mapping between objects in programming languages and relational database systems. This involves creating objects based on the data in the database and defining the relationship between the data in the database and the objects created. For example, an ORM might create a user object based on the data contained in the “users” table, and map the fields of the table to the user object’s properties. Using ORM reduces the tediousness of having to manually translate data between the database and the application’s objects.
-react js and electron js
-
-React.js is a JavaScript library for building user interfaces, and is used for creating web applications. It is designed to be a superior way to break down complex UI elements into reusable components. It is also open source and used by many popular websites, such as Facebook, Instagram, and Airbnb.
-
-Electron.js is an open-source framework for building desktop applications with web technologies, such as HTML, CSS, and JavaScript. It allows developers to write cross-platform desktop apps using JavaScript, HTML, and CSS. Its main purpose is to provide an application runtime environment that allows developers to create native applications for Windows, OSX, and Linux.
-electron.js code to connect to a database
-
 const {app, BrowserWindow} = require('electron');
 const mysql = require('mysql');
 
