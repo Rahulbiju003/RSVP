8709a0bc773ddc62cfe8093e1e5e1b5876f4a006
Shreejit03 <116968835+Shreejit03@users.noreply.github.com>
2023-05-27 11:44:37 +0530

    Update conn.php

--- phps/conn.php
+++ phps/conn.php
@@ -1,7 +1,6 @@
 <?php
 $conn = mysqli_connect("localhost","root","","basic");
  
-// Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
